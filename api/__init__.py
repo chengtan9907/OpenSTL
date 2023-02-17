40ed20e64bbdfb3290be9c7ad1df556e99bea6bf
Lupin1998 <1070535169@qq.com>
2023-02-17 22:18:43 +0000

    clean up

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dataloader_moving_mnist import MovingMNIST
-from .dataloader_kitticaltech import KittiCaltechDataset
-from .dataloader import load_data
-from .metrics import metric
-from .recorder import Recorder
-
-__all__ = [
-    'MovingMNIST', 'KittiCaltechDataset', 'load_data', 'metric', 'Recorder',
-]
