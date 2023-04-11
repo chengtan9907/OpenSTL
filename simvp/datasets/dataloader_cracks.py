0c00fb709c00d1a22acb9c7205f62d1917750654
pyrothic <56771722+pyrothic@users.noreply.github.com>
2023-04-11 15:14:45 -0400

    fixing dataloader so entire models run

--- simvp/datasets/dataloader_cracks.py
+++ simvp/datasets/dataloader_cracks.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
-from .video_dataset import VideoFolder, video_loader
+from .video_dataset import VideoFolder, video_loader, make_dataset, find_videos
 import torchvision.transforms as transforms
 
 
@@ -15,42 +15,50 @@ def load_data(batch_size, val_batch_size, data_root, num_workers=4, pre_seq_leng
     transform_video = transforms.Compose([transforms.Resize(
         size=(128, 128), interpolation=Image.NEAREST), transforms.ToTensor(),])
 
-    train_set = VideoFolder(video_root=train_set_path, video_ext='png',
+    train_set = Cracks(video_root=train_set_path, video_ext='png',
                             nframes=6, loader=video_loader, transform=transform_video)
-    test_set = VideoFolder(video_root=test_set_path, video_ext='png',
+    test_set = Cracks(video_root=test_set_path, video_ext='png',
                            nframes=6, loader=video_loader, transform=transform_video)
+    
 
-    dataloader_train = torch.utils.data.DataLoader(
-        train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, generator=torch.Generator(device='cpu'))
-#            train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    dataloader_train = torch.utils.data.DataLoader(train_set,
+                                                   batch_size=batch_size, shuffle=True,
+                                                   pin_memory=True, drop_last=True,
+                                                   num_workers=num_workers)
+    dataloader_vali = None
+                        # torch.utils.data.DataLoader(test_set,
+                        #                           batch_size=val_batch_size, shuffle=False,
+                        #                           pin_memory=True, drop_last=True,
+                        #                           num_workers=num_workers)
+    dataloader_test = torch.utils.data.DataLoader(test_set,
+                                                  batch_size=val_batch_size, shuffle=False,
+                                                  pin_memory=True, drop_last=True,
+                                                  num_workers=num_workers)
+    
+    return dataloader_train, dataloader_vali, dataloader_test
 
-    dataloader_validation = None
-
-    dataloader_test = torch.utils.data.DataLoader(
-        test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers, generator=torch.Generator(device='cpu'))
-#            test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    return dataloader_train, dataloader_validation, dataloader_test
 
 
-class Cracks(Dataset):
+class Cracks(VideoFolder):
 
     # copying from taxibj to see essential methods
 
-    def __init__(self, X, Y):
-        super(Cracks, self).__init__()
-        self.X = (X+1)/2
-        self.Y = (Y+1)/2
+    def __init__(self, video_root, video_ext, nframes, loader=video_loader, transform=None):
+        video_names, video_idx = find_videos(video_root)
+        video_dataset = make_dataset(video_root, video_ext, nframes)
+        
+        self.root = video_root
+        self.ext = video_ext
+        self.nframes = nframes
+        self.loader = loader
+        
+        self.video_names = video_names
+        self.video_idx = video_idx
+        self.video_dataset = video_dataset
+
+        self.transform = transform
         self.mean = 0
         self.std = 1
-
-    def __len__(self):
-        return self.X.shape[0]
-
-    def __getitem__(self, index):
-        data = torch.tensor(self.X[index, ::]).float()
-        labels = torch.tensor(self.Y[index, ::]).float()
-        return data, labels
-    
 
 # below is load data from taxibj
 
