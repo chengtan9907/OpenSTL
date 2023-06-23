3434e0a12c243dfc768fb3956d421327f3940ec3
Cheng Tan <tancheng@westlake.edu.cn>
2023-06-23 09:57:37 +0000

    fix a minor bug in dataloader

--- openstl/datasets/dataloader.py
+++ openstl/datasets/dataloader.py
@@ -20,8 +20,9 @@ def load_data(dataname, batch_size, val_batch_size, num_workers, data_root, dist
     elif 'kth' in dataname:  # 'kth', 'kth20', 'kth40'
         from .dataloader_kth import load_data
         return load_data(batch_size, val_batch_size, data_root, num_workers, **cfg_dataloader)
-    elif 'kth' in dataname:  # 'k400', 'kth20',
-        from .dataloader_kth import load_data
+    elif 'mnist' in dataname:  # 'mmnist', 'mfmnist'
+        from .dataloader_moving_mnist import load_data
+        cfg_dataloader['data_name'] = kwargs.get('data_name', 'mnist')
         return load_data(batch_size, val_batch_size, data_root, num_workers, **cfg_dataloader)
     elif 'kinetics' in dataname:  # 'kinetics400', 'kinetics600'
         from .dataloader_kinetics import load_data
