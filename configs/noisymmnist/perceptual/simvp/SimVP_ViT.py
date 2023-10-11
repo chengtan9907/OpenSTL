0d7fe0c0b970aae7d656913d4baac1756d674a6c
Cheng Tan <tancheng@westlake.edu.cn>
2023-10-11 05:45:57 +0000

    add noisymmnist dataset for robustness evalulation

---
+++ configs/noisymmnist/perceptual/simvp/SimVP_ViT.py
@@ -0,0 +1,16 @@
+method = 'SimVP'
+# model
+spatio_kernel_enc = 3
+spatio_kernel_dec = 3
+model_type = 'vit'
+hid_S = 64
+hid_T = 512
+N_T = 8
+N_S = 4
+# training
+lr = 1e-3
+batch_size = 16
+drop_path = 0
+sched = 'onecycle'
+# noisy type
+noise_type = 'perceptual'
