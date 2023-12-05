bb81b3c863f644402bd744f4d6e9c3fd2ad154e4
54wb <1225768798@qq.com>
2023-12-05 21:04:01 +0800

    add SwinLSTM model

--- openstl/models/__init__.py
+++ openstl/models/__init__.py
@@ -12,9 +12,10 @@ from .predrnnpp_model import PredRNNpp_Model
 from .predrnnv2_model import PredRNNv2_Model
 from .simvp_model import SimVP_Model
 from .dmvfn_model import DMVFN_Model
+from .swinlstm_model import SwinLSTM_B_Model, SwinLSTM_D_Model
 
 __all__ = [
     'ConvLSTM_Model', 'CrevNet_Model', 'E3DLSTM_Model', 'MAU_Model', 'MIM_Model',
     'PhyDNet_Model', 'PredNet_Model', 'PredRNN_Model', 'PredRNNpp_Model', 'PredRNNv2_Model', 'SimVP_Model',
-    'DMVFN_Model'
+    'DMVFN_Model', 'SwinLSTM_B_Model', 'SwinLSTM_D_Model'
 ]
