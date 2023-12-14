f03ada2793a02ba70fdb2fca01c68eb0e5682fe8
Cheng Tan <tancheng@westlake.edu.cn>
2023-12-14 08:30:55 +0000

    add MMVP config for moving-mnist dataset

---
+++ configs/mmnist/MMVP.py
@@ -0,0 +1,13 @@
+method = 'MMVP'
+# model
+downsample_setting = '2,2,2'
+hid_S = 32
+hid_T = 192
+rrdb_encoder_num = 2
+rrdb_decoder_num = 2
+rrdb_enhance_num = 2
+use_direct_predictor = True
+# training
+lr = 1e-3
+batch_size = 16
+sched = 'onecycle'
